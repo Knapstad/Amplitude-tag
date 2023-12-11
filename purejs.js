1e4e60ac2e0c59799d98e467a0854c8b4f0ed882
Bendik Knapstad <b.knapstad@gmail.com>
2023-12-11 12:43:15 +0100

    fisrt commit

---
+++ purejs.js
@@ -0,0 +1,54 @@
+const sendHttpRequest = require('sendHttpRequest');
+const getAllEventData = require('getAllEventData');
+const makeInteger = require('makeInteger');
+const makeTableMap = require('makeTableMap');
+const JSON = require('JSON');
+const getRequestHeader = require('getRequestHeader');
+const logToConsole = require('logToConsole');
+const getContainerVersion = require('getContainerVersion');
+const containerVersion = getContainerVersion();
+const isDebug = containerVersion.debugMode;
+const logging = loggingEnabled();
+const setResponseBody = require("setResponseBody");
+const postHeaders = { 'Content-Type': 'application/json' };
+let postBodyData = {};
+
+const eventData = getAllEventData();
+const amplitudeBody = JSON.stringify(eventData);
+
+let requestOptions = { headers: postHeaders, method: data.requestMethod };
+
+if (data.timeout) {
+    requestOptions.timeout = makeInteger(data.timeout);
+}
+
+if (logging) {
+    logToConsole("Amplitude Request Body: " + amplitudeBody);
+}
+
+sendHttpRequest(data.url, (statusCode, headers, body) => {
+
+
+    if (statusCode >= 200 && statusCode < 300) {
+        setResponseBody(body);
+        data.gtmOnSuccess();
+    } else {
+        data.gtmOnFailure();
+    }
+}, requestOptions, amplitudeBody);
+
+function loggingEnabled() {
+    if (!data.logType) {
+        return isDebug;
+    }
+
+    if (data.logType === 'no') {
+        return false;
+    }
+
+    if (data.logType === 'debug') {
+        return isDebug;
+    }
+
+    return data.logType === 'always';
+}
